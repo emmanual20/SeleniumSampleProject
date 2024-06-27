ebe80f25d1ba2ed62c62c85ee76f5f4c56a35525
jyoti <jyoti@LAPTOP-LPR232VR>
2024-06-27 17:50:04 +0530

    updates

--- SeleniumProject/src/test/java/pac2/OrgTest.java
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -6,6 +6,11 @@ public class OrgTest {
 	@Test
 public void createorgTest() {
 	System.out.println("execute create Org Test");
+	String url=System.getProperty("url");
+	String browser=System.getProperty("browser");
+	String username=System.getProperty("login");
+	String password=System.getProperty("password");
+	System.out.println(url+","+browser+","+username+","+password);
 }
 
 @Test
